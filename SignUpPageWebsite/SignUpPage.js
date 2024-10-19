705130a7ca8b30c187be78593fb50686416cea63
AustinBarikdar <austinbarikdar@gmail.com>
2024-10-18 21:01:28 -0400

    Changed to php for dataserivce

--- SignUpPageWebsite/SignUpPage.js
+++ SignUpPageWebsite/SignUpPage.js
@@ -1,46 +1,42 @@
 let SignUpEmailLabel = document.getElementById('EmailLabel'); 
-let SignUpEmailActualLabel= document.getElementById('EmailActualLabel');
 let EmailInput = document.getElementById('EmailInput');
-EmailInput.onmouseenter = function(){
-    SignUpEmailLabel.style.bottom = "-13px";
-    SignUpEmailLabel.style.left = "23px";
 
-
-
-};
-
-EmailInput.onmouseleave = function(){
-    let EmailInputValue = EmailInput.value;
-    if (EmailInputValue === ""){
-        SignUpEmailLabel.style.bottom = "-33px";
-        SignUpEmailLabel.style.left = "23px";
+function onmouseleavefunction(Input,Label){
+    let InputFieldValue = Input.value;
+    if (InputFieldValue === ""){
+        Label.style.bottom = "-33px";
+        Label.style.left = "23px";
 
     }
+}
+
+function onmouseenterfunction(Label){
+    Label.style.bottom = "-13px";
+    Label.style.left = "23px";
+}
+
+function onkeydownfunction(Input,Label){
+    let InputValue = Input.value;
+    if (InputValue !== ""){
+        Label.style.bottom = "-13px";
+        Label.style.left = "23px";
+   
+    }else{
+        Label.style.bottom = "-33px";
+        Label.style.left = "23px";  
+    }
+}
+
+EmailInput.onmouseenter = function(){
+    onmouseenterfunction(SignUpEmailLabel)
+}
+
+
+EmailInput.onmouseleave = function(){
+    onmouseleavefunction(EmailInput,SignUpEmailLabel)
 
 };
 EmailInput.onkeydown = function(){
-    let EmailInputValue = EmailInput.value;
-    if (EmailInputValue !== ""){
-        SignUpEmailLabel.style.bottom = "-13px";
-        SignUpEmailLabel.style.left = "23px";
-   
-    }else{
-        SignUpEmailLabel.style.bottom = "-33px";
-        SignUpEmailLabel.style.left = "23px";  
-    }
+    onkeydownfunction(EmailInput,SignUpEmailLabel)
 
-};
-
-function MouseEnter(InputField,Label){
-    Label.style.bottom = "-13px";
-    Label.style.left = "23px";
-};
-
-function MouseLeave(InputField,Label){
-    let LabelValue = Label.value;
-    if (LabelValue === ""){
-        SignUpEmailLabel.style.bottom = "-33px";
-        SignUpEmailLabel.style.left = "23px";
-
-    }
 };
